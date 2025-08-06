1b25009959e79d5e9df23101c66f56182c030b4a
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-08-06 14:08:57 +0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,13 @@
-from flask import Blueprint, render_template, jsonify, request, send_file
+from flask import Blueprint, render_template, jsonify
 from app.utils.scraper import scrape_railway_news
-from app.utils.pdf_generator import create_pdf
 
 main_routes = Blueprint('main', __name__)
 
 @main_routes.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # Artıq base.html istifadə etmir
 
 @main_routes.route('/search_news')
 def search_news():
     news = scrape_railway_news()
     return jsonify(news)
-
-@main_routes.route('/export_pdf', methods=['POST'])
-def export_pdf():
-    news_data = request.json
-    filename = create_pdf(news_data)
-    return send_file(filename, as_attachment=True)

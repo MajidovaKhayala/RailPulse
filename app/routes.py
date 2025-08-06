fdc9f384b6b83492382d5342b7945e9a7bd3319a
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-08-06 13:34:21 +0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,20 @@
-from flask import render_template, request, jsonify, send_file
+from flask import Blueprint, render_template, jsonify, request, send_file
 from app.utils.scraper import scrape_railway_news
 from app.utils.pdf_generator import create_pdf
 
-def init_app(app):
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+main_routes = Blueprint('main', __name__)
 
-    @app.route('/search_news')
-    def search_news():
-        news = scrape_railway_news()
-        return jsonify(news)
+@main_routes.route('/')
+def index():
+    return render_template('index.html')
 
-    @app.route('/export_pdf', methods=['POST'])
-    def export_pdf():
-        news_data = request.json
-        filename = create_pdf(news_data)
-        return send_file(filename, as_attachment=True)
+@main_routes.route('/search_news')
+def search_news():
+    news = scrape_railway_news()
+    return jsonify(news)
+
+@main_routes.route('/export_pdf', methods=['POST'])
+def export_pdf():
+    news_data = request.json
+    filename = create_pdf(news_data)
+    return send_file(filename, as_attachment=True)

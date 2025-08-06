304da80d88e79a1576b00be5903eba89afd73b60
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-08-06 15:48:17 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,24 @@
+# Bu faylda yalnız bunlar olmalıdır:
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     
-    # Configuration
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    
-    # Import and register blueprints/routes
     from app.routes import main_routes
     app.register_blueprint(main_routes)
     
     return app
+
+# from flask import Flask
+
+# def create_app():
+#     app = Flask(__name__)
+    
+#     # Configuration
+#     app.config['SECRET_KEY'] = 'your-secret-key'
+    
+#     # Import and register blueprints/routes
+#     from app.routes import main_routes
+#     app.register_blueprint(main_routes)
+    
+#     return app

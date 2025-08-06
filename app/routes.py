af01a582b35ecc0addcfa3e4ebd103ff1950f250
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-08-06 15:37:50 +0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,32 @@
-from flask import Blueprint, render_template, jsonify
-from app.utils.scraper import scrape_railway_news
-
-main_routes = Blueprint('main', __name__)
-
-@main_routes.route('/')
-def index():
-    return render_template('index.html')
+from app.utils.scraper import scrape_news
 
 @main_routes.route('/search_news')
 def search_news():
-    try:
-        news = scrape_railway_news()
-        return jsonify({"success": True, "news": news})
-    except Exception as e:
-        return jsonify({"success": False, "error": str(e)})
+    news = scrape_news()
+    return jsonify({
+        "status": "success",
+        "count": len(news),
+        "sources": list(set([n['source'] for n in news])),  # Aktiv mənbələr
+        "news": news
+    })
+
+
+# from flask import Blueprint, render_template, jsonify
+# from app.utils.scraper import scrape_railway_news
+
+# main_routes = Blueprint('main', __name__)
+
+# @main_routes.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @main_routes.route('/search_news')
+# def search_news():
+#     try:
+#         news = scrape_railway_news()
+#         return jsonify({"success": True, "news": news})
+#     except Exception as e:
+#         return jsonify({"success": False, "error": str(e)})
 
 
 # from flask import Blueprint, render_template, jsonify
